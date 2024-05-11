1c1a884ccfe575a375a5c7fcea1315832d63ba23
Danny Boushka <boushka9@gmail.com>
2024-05-11 14:08:18 -0600

    Updated form with Formspree

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,22 +6,22 @@
             var instructionsText = "";
 
             switch (category) {
-                case "supperClub":
+                case "Supper_Club":
                     instructionsText = "Please specify which meal options you would like for the week (up to FIVE MEALS) and for how many people (2-3 or 4-5). If you would like to include any 'Add-Ons' or dessert options please include those in your message.";
                     break;
-                case "breakfast":
+                case "Breakfast":
                     instructionsText = "Please specify if you would like 'Breakfast In A Box', or the 'Buffet' options and how many people to prepare for. Please specify your desired drink options and if you would like any 'Add-Ons'.";
                     break;
-                case "desserts":
+                case "Desserts":
                     instructionsText = "Please specify which dessert options you would like and how many.";
                     break;
-                case "inFlight":
+                case "In_Flight":
                     instructionsText = "Please specify which menu options you would like (Breakfast, Shareables, Afternoon Tea) and how many people will be on flight.";
                     break;
-                case "charcuterie":
+                case "Charcuterie_Boards":
                     instructionsText = "Please specify which size Charcuterie board you would like and what image. ";
                     break;
-                case "seasonal":
+                case "Seasonal":
                     instructionsText = "Please specify desired quantity of seasonal cookies.";
                     break;
                 
